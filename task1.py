b57663c89ef393a37a010d600c0fffc10aafb6af
VasperJusper <88136510+VasperJusper@users.noreply.github.com>
2024-03-30 10:09:57 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,7 +1,6 @@
 # считывание файла и создание списка элементов
 inp = list(map(lambda x: x.strip().split('*'), open('space.txt', encoding='utf-8').readlines()))
 out = open('space_new.txt', encoding='utf-8', mode="w")
-out.write('ShipName*planet*coord_place*direction\n')
 
 # функция "починки" данных
 
@@ -24,13 +23,14 @@ for e in inp[1:]:
     m = int(e[0].split('-')[1][1])
     t = len(e[1])
     x, y = map(int, e[3].split())
+    coords = 0
 
-    # замена нулей
     if e[2].split()[0] == '0' and e[2].split()[1] == '0':
-        e[2] = ' '.join(f(n, m, t, x, y))
+        coords = f(n, m, t, x, y)
     elif e[2].split()[0] == '0':
-        e[2] = ' '.join([f(n, m, t, x, y)[0], e[2].split()[1]])
+        coords = f(n, m, t, x, y)[0], e[2].split()[1]
     elif e[2].split()[1] == '0':
-        e[2] = ' '.join([e[2].split()[0], f(n, m, t, x, y)[1]])
-    # запись в файл
-    out.write('*'.join(e) + '\n')
+        coords = e[2].split()[0], f(n, m, t, x, y)[1]
+
+    if coords and e[0].split('-')[0][-1] == 'V':
+        out.write(f"{e[1]} - ({coords[0]}, {coords[1]})\n")
